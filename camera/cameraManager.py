c16039596dc6612a00d3ce9bef62dfee137f4862
Sumin <psm1st@sookmyung.ac.kr>
2025-07-17 15:52:31 +0900

    design : 카메라 원래대로 보이게 위치 조정

--- camera/cameraManager.py
+++ camera/cameraManager.py
@@ -1,15 +1,14 @@
 from picamera2 import Picamera2
 from libcamera import Transform
 
-def create_camera(index: int, rotation: int) -> Picamera2:
+def create_camera(index: int) -> Picamera2:
     cam = Picamera2(index)
     cam.configure(cam.create_video_configuration(
         main={"size": (640, 480), "format": "RGB888"},
-        transform=Transform(rotation=rotation)
     ))
     cam.set_controls({"AwbMode": 0, "ColourGains": (1.5, 1.9)})
     cam.start()
     return cam
 
-picam0 = create_camera(0, 270)
-picam1 = create_camera(1, 90)
+picam0 = create_camera(0)
+picam1 = create_camera(1)
