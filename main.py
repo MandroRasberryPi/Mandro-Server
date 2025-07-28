495459d18a11b8b94fd0d485d699111d75057de5
Sumin <psm1st@sookmyung.ac.kr>
2025-07-28 02:43:18 +0100

    feat : cors 설정

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, WebSocket
 from routes.index import get_index_page
 from websocket.endpoints import stream_camera
 from camera.cameraManager import picam0, picam1
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -16,3 +17,11 @@ async def websocket_cam0(websocket: WebSocket):
 @app.websocket("/ws1")
 async def websocket_cam1(websocket: WebSocket):
     await stream_camera(websocket, picam1)
+    
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
