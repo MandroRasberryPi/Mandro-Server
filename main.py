103b4602e7fbc6a5acdf2306bb642035870a10e8
Sumin <psm1st@sookmyung.ac.kr>
2025-07-30 05:57:57 +0100

    feat : camera update api 변수 받아오기까지 완료

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, WebSocket,Form
 from routes.index import get_index_page
 from websocket.endpoints import stream_camera
 from camera.cameraManager import picam0, picam1
 from fastapi.middleware.cors import CORSMiddleware
+from camera.cameraState import update_camera_state
 
 app = FastAPI()
 
@@ -17,7 +18,20 @@ async def websocket_cam0(websocket: WebSocket):
 @app.websocket("/ws1")
 async def websocket_cam1(websocket: WebSocket):
     await stream_camera(websocket, picam1)
-    
+
+@app.post("/update")
+async def update_values(
+    left: int = Form(...),
+    right: int = Form(...),
+    distorted: str = Form(...)
+):
+    try:
+        update_camera_state(left, right, distorted) 
+        return {"message": "Values updated successfully"}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  
